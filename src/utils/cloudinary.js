1cd14eaba9e3c2d2153a9b4f205b5b348b2c0c41
Aaryen D'souza <dc.aaryen@gmail.com>
2025-01-16 09:03:47 -0500

    created file uploda middleware configs

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
+
+(async function () {
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+})();
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File uploaded successfully", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(local);
+
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
