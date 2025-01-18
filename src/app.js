1e6d8898f3ce4e5b1579c6cb9ab6342db9750ce8
Aaryen D'souza <dc.aaryen@gmail.com>
2025-01-18 02:12:55 -0500

    Config done

--- src/app.js
+++ src/app.js
@@ -12,11 +12,14 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" })); // to let express know that json data is coming
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // to let express know that url encoded data is coming
-
 app.use(express.static("public")); // to serve static files
-
 app.use(cookieParser()); // to parse cookies securely from server to client
+
+// route imports
+import userRoutes from "./routes/user.routes.js";
+
+// route middlewares
+app.use("/api/v1/users", userRoutes);
 
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,10 @@
 const asyncHandler = (reqHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err));
   };
 };
+
+export { asyncHandler };
 
 // asyncHandler is a wrapper function which accepts fn function as argument then req, res and next as arguments are passed to the fn function to use it when fn function is called. If there is an error in the fn function, it will be caught and the error message will be sent as a response to the client with the status code of the error. This way, we don't have to write try-catch blocks in every route handler function.
 
